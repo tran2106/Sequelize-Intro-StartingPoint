1e541dce1bda223734a13daf12132ba6e4e1bfaf
Finn Terdal <finnterdal@gmail.com>
2025-06-27 15:02:32 -0400

    change db name

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/ttp_backend",
+  process.env.DATABASE_URL || "postgres://localhost:5432/todo_list",
   {
     logging: false, // comment this line to enable logging
   }
