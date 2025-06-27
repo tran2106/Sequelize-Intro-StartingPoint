31d68369d1aa3b02779652cfcaef69b123091c1a
Finn Terdal <finnterdal@gmail.com>
2025-06-27 13:30:10 -0400

    change database connection string

--- database/db.js
+++ database/db.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
-const db = new Sequelize("postgres://localhost:5432/todo_list", {
-  logging: false, // comment this line to enable logging
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/ttp_backend",
+  {
+    logging: false, // comment this line to enable logging
+  }
+);
 
 module.exports = db;
